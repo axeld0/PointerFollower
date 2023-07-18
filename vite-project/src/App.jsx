fd89bfed0f157fd4723423ea8c518527a51a73a9
axeld0 <axel.do@live.com.ar>
2023-07-18 16:43:05 -0300

    running

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,66 @@
+import { useEffect, useState } from "react"
+
+
+const FollowMouse = () =>{
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState ({ x: 0 , y : 0 })
+
+
+  useEffect(() =>{
+
+    console.log('effect', {enabled})
+
+    const handleMove = (event) => {
+      const{clientX, clientY} = event
+      console.log('handleMove', {clientX, clientY})
+      setPosition({ x: clientX , y : clientY})
+    }
+
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+ 
+
+
+    return () =>{
+      window.removeEventListener('pointermove', handleMove)
+    } //it executes when useEffect is no longer needed
+  }), [enabled]
+
+  return(
+  <>
+      <div style= {{
+            position: "absolute",
+            backgroundColor : '#09f',
+            borderRadius : '50%',
+            opacity : 0.8 ,
+            pointerEvents : 'none',
+            left : -20, 
+            top : -20,
+            width : 40,
+            height : 40,
+            transform : `translate(${position.x}px,${position.y}px`
+      }
+      } />
+      <button onClick={() => setEnabled (!enabled)}>
+        {enabled ? 'Deactivate' : 'Activate'} effect
+      </button>
+    </>
+  )
+}
 
 function App() {
+
+  const [mounted, setMounted] =useState(true)
   return (
-    <>
-      <h1>BOCA EL MAS GRANDE</h1> 
-    </>
+    
+    <main>
+      {mounted && <FollowMouse />}
+      <button onClick={()=>setMounted(!mounted)}>
+        Toggle mounted FollowPointer component
+      </button>
+    </main>
   )
 }
 
